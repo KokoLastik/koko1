da18d5f45cf46860dc021b447f1173e23c0780d5
KokoLastik <77006789+KokoLastik@users.noreply.github.com>
2021-01-09 17:08:04 +0800

    Add files via upload

---
+++ koko1/[hud]/in_active/esx_basicneeds.sql
@@ -0,0 +1,6 @@
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('bread', 'Pain', 10),
+	('water', 'Eau', 5)
+;
---
+++ koko1/[hud]/in_active/localization/sv_esx_basicneeds.sql
@@ -0,0 +1,6 @@
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('bread', 'Bröd', 10),
+	('water', 'Vatten', 5)
+;
